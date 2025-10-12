dd56313aa14884956a56d881a84cace1d1a85583
faris Naser <solonaser9@gmail.com>
2025-10-11 18:03:06 -0700

    navigation pages

--- src/componts/AntiThef.jsx
+++ src/componts/AntiThef.jsx
@@ -1,7 +1,37 @@
 import React from "react";
 
 function AntiThef() {
-  return <div>AntiThef</div>;
+  return (
+    <>
+      <div>
+        <nav>
+          <div>
+            <div>
+              <img src="" alt="" />
+            </div>
+
+            <div>
+              <h2>Anti-Theft Protection</h2>
+              <h4>Device tracking, remote control, and theft prevention</h4>
+            </div>
+          </div>
+
+          <div>
+            <button>
+              {" "}
+              <img src="" alt="" />
+              <p>Locate Device</p>
+            </button>
+            <button>
+              {" "}
+              <img src="" alt="" />
+              <p>Emergancy</p>
+            </button>
+          </div>
+        </nav>
+      </div>
+    </>
+  );
 }
 
 export default AntiThef;
