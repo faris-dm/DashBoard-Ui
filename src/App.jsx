878edf4448d8e1a6d57dc11913eb5bf56debfce7
faris Naser <solonaser9@gmail.com>
2025-10-11 17:56:22 -0700

    start the AntiTheft PAge

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
-// import Dashboard from "./componts/Dashboard";
+import Dashboard from "./componts/Dashboard";
 import Threat from "./componts/threatPage";
 import DataValue from "./componts/DataValuet";
 import Security from "./componts/security";
 import Network from "./componts/Network";
+import Theif from "./componts/AntiThef";
 
 import "./App.css";
 
@@ -11,10 +12,11 @@ function App() {
   return (
     <>
       {/* <Dashboard /> */}
-      <Threat />
+      {/* <Threat /> */}
       {/* <DataValue /> */}
       {/* <Security /> */}
       {/* <Network /> */}
+      <Theif />
     </>
   );
 }
