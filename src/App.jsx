1e6fefccc6fc59d9b32c46ec73fa65fa9357f29f
faris Naser <solonaser9@gmail.com>
2025-10-16 15:36:20 -0700

    svg setup

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
   return (
     <>
       {/* <Dashboard /> */}
-      <Threat />
+      {/* <Threat /> */}
       {/* <DataValue /> */}
       {/* <Security /> */}
       {/* <Network /> */}
