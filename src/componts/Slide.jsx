8d468761d9ca41cba9de429c15c64c3a62a6b6d2
faris Naser <solonaser9@gmail.com>
2025-10-19 17:21:49 -0700

    adding the active and hover effect in the slide bar

--- src/componts/Slide.jsx
+++ src/componts/Slide.jsx
@@ -37,14 +37,14 @@ function Slide() {
           <p className=" border-t  border-silver-100"></p>
           <p className=" pt-3 pb-2 px-2">Security Center</p>
           <div
-            onClick={ClickChange}
-            className={`flex items-center gap-2 p-3 ${
-              clicked ? "bg-cyan-950 rounded-xl" : ""
+            onClick={() => isClicked(1)}
+            className={`flex items-center gap-2 p-3 hover:bg-cyan-950 hover:rounded-xl  ${
+              clicked === 1 ? "bg-cyan-700 rounded-xl" : ""
             }`}
           >
             <House
               className={`  ${
-                clicked ? "text-[#22d3ee]  " : "text-[#b0b0b0]"
+                clicked === 1 ? "text-[#22d3ee]  " : "text-[#b0b0b0]"
               } `}
             />
             <h2 className="text-[#b0b0b0]  font-medium    text-sm capitalize ">
@@ -53,14 +53,14 @@ function Slide() {
           </div>
           {/* className="" dashboard */}
           <div
-            onClick={ClickChange}
-            className={`flex items-center gap-2 p-3 mt-2 ${
-              clicked ? "bg-cyan-950 rounded-xl" : ""
+            onClick={() => isClicked(2)}
+            className={`flex items-center gap-2 p-3 mt-2 hover:bg-cyan-950 hover:rounded-xl  ${
+              clicked === 2 ? "bg-cyan-700 rounded-xl" : ""
             }`}
           >
             <ShieldAlert
               className={`  ${
-                clicked ? "text-[#22d3ee]  rounded-xl" : "text-[#b0b0b0]"
+                clicked === 2 ? "text-[#22d3ee]  rounded-xl" : "text-[#b0b0b0]"
               } `}
             />
             <h2 className="text-[#b0b0b0]    text-sm font-medium capitalize ">
@@ -72,14 +72,14 @@ function Slide() {
 ✅ :active → when the element is clicked or pressed */}
           {/* Therat Detection */}
           <div
-            onClick={ClickChange}
-            className={`flex items-center gap-2 p-3 mt-2 ${
-              clicked ? "bg-cyan-950 rounded-xl" : ""
+            onClick={() => isClicked(3)}
+            className={`flex items-center gap-2 p-3 mt-2 hover:bg-cyan-950 hover:rounded-xl  ${
+              clicked === 3 ? "bg-cyan-700 rounded-xl" : ""
             }`}
           >
             <Lock
               className={`  ${
-                clicked ? "text-[#22d3ee]  " : "text-[#b0b0b0]"
+                clicked === 3 ? "text-[#22d3ee]  " : "text-[#b0b0b0]"
               } `}
             />
             <h2 className="text-[#b0b0b0]    text-sm capitalize font-medium ">
@@ -89,14 +89,14 @@ function Slide() {
           {/* Data Valuet */}
           {/* Network Protection */}
           <div
-            onClick={ClickChange}
-            className={`flex items-center gap-2 p-3 mt-2 ${
-              clicked ? "bg-cyan-950 rounded-xl" : ""
+            onClick={() => isClicked(4)}
+            className={`flex items-center gap-2 p-3 mt-2 hover:bg-cyan-950 hover:rounded-xl  ${
+              clicked === 4 ? "bg-cyan-700 rounded-xl" : ""
             }`}
           >
             <Smartphone
               className={`  ${
-                clicked ? "text-[#22d3ee]  rounded-xl" : "text-[#b0b0b0]"
+                clicked === 4 ? "text-[#22d3ee]  rounded-xl" : "text-[#b0b0b0]"
               } `}
             />
             <h2 className="text-[#b0b0b0]    text-sm capitalize font-medium ">
@@ -105,14 +105,14 @@ function Slide() {
           </div>
           {/* Anti theif */}
           <div
-            onClick={ClickChange}
-            className={`flex items-center gap-2 p-3 mt-2 ${
-              clicked ? "bg-cyan-950 rounded-xl" : ""
+            onClick={() => isClicked(5)}
+            className={`flex items-center gap-2 p-3 mt-2 hover:bg-cyan-950 hover:rounded-xl  ${
+              clicked === 5 ? "bg-cyan-700 rounded-xl" : ""
             }`}
           >
             <Wifi
               className={`  ${
-                clicked ? "text-[#22d3ee]  rounded-xl" : "text-[#b0b0b0]"
+                clicked === 5 ? "text-[#22d3ee]  rounded-xl" : "text-[#b0b0b0]"
               } `}
             />
             <h2 className="text-[#b0b0b0]    text-sm capitalize font-medium ">
@@ -121,14 +121,14 @@ function Slide() {
           </div>
           {/* setting */}
           <div
-            onClick={ClickChange}
-            className={`flex items-center gap-2 p-3 mt-2 ${
-              clicked ? "bg-cyan-950 rounded-xl" : ""
+            onClick={() => isClicked(6)}
+            className={`flex items-center gap-2 p-3 mt-2 hover:bg-cyan-950 hover:rounded-xl  ${
+              clicked === 6 ? "bg-cyan-700 rounded-xl" : ""
             }`}
           >
             <Settings
               className={`  ${
-                clicked ? "text-[#22d3ee]  rounded-xl" : "text-[#b0b0b0]"
+                clicked === 6 ? "text-[#22d3ee]  rounded-xl" : "text-[#b0b0b0]"
               } `}
             />
             <h2 className="text-[#b0b0b0]    text-sm capitalize font-medium ">
